56861362924a73a73e9f2b1f68840e0fad390618
Makaela <mharrell1@my.hpu.edu>
2023-09-25 04:33:39 +0000

    Finish toy app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,3 @@
 class Micropost < ApplicationRecord
-  belongs_to :user
   validates :content, length: { maximum: 140 }
-                      # Ensures that microposts cannot be blank
-                      presence: true
 end
